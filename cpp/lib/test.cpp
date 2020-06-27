5fb074b9155a78678424e2731093683f9364895e
yashsehgal <yashsehgal.work@gmail.com>
2020-06-27 22:10:34 +0530

    test file for testing BasicToolSet C++ features and functions

---
+++ cpp/lib/test.cpp
@@ -0,0 +1,10 @@
+// #include <MathTools/BasicToolSet.h>
+// #include <iostream>
+
+
+// int main() {
+//   BasicToolSet basicToolSet;
+//   std::cout << "> " << basicToolSet.addNum(2, 3) << std::endl;
+//   std::cout << "> " << basicToolSet.addBoolean(true, true) << std::endl;
+//   return 0;
+// }
