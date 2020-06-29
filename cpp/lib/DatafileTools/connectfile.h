8854dd69c8933f9ba5af0c85ae3cac0b9fac6f92
yashsehgal <yashsehgal.work@gmail.com>
2020-06-29 23:12:20 +0530

    datafile handling modules --first-commit

---
+++ cpp/lib/DatafileTools/connectfile.h
@@ -0,0 +1,42 @@
+#ifndef _CONNECT_FILE__SPEECH_TOOLS__
+#define _CONNECT_FILE__SPEECH_TOOLS__
+
+#include <iostream>
+#include <string>
+#include <fstream.h>
+
+class ConnectFile {
+  private:
+  char * fileMode;
+  char * fileType;
+  int length;
+  int wordCount;
+  char * filename;
+
+  public:
+  ConnectFile() {
+    filename = '\0';
+  }
+  ConnectFile(char * filename, char * fileMode = "in", char * fileType = "bin") {
+    if (filename == '\0') {
+      std::cout << "Please give the file name in order to open it!" << std::endl;
+    }
+    else if (checkFileStatus(filename, fileMode, fileType)) {
+
+    }
+  }
+  bool checkFileStatus(char * filename, char * mode, char * type) {
+    if (mode == "in") {
+      if (type == "bin") {
+        ifstream fileObject;
+        fileObject.open(filename, ios::in | ios::bin);
+      }
+      else if (type == "ate") {
+        ifstream fileObject;
+      }
+    }
+  }
+  protected:
+};
+
+#endif // _CONNECT_FILE__SPEECH_TOOLS__
