fcb1d5914b39a6870b5653156c5197219408412c
yashsehgal <yashsehgal.work@gmail.com>
2020-06-29 17:26:43 +0530

    LinearAlgebra modules in C++ --latest-commit

---
+++ cpp/lib/MathTools/AdvancedTools/LinearAlgebra.cc
@@ -0,0 +1,3 @@
+#include <LinearAlgebra.h>
+
+// .cc module
---
+++ cpp/lib/MathTools/AdvancedTools/LinearAlgebra.h
@@ -0,0 +1,40 @@
+#ifndef _LINEAR_ALGEBRA_MATH_TOOLS__
+#define _LINEAR_ALGEBRA_MATH_TOOLS__
+
+#include <iostream>
+#include <string>
+
+class LinearAlgebra {
+  private:
+    char *mode;
+  public:
+    LinearAlgebra(char * command = "basic") {
+      if (mode == "advanced") {
+        mode = command;
+      }
+      else {
+        std::cout << "use valid mode!" << std::endl;
+      }
+    }
+    double *getCoordinates_XY(double a1, double a2,
+      double b1, double b2, double c1, double c2) {
+        double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
+        double xValue = ((c1 - (b1 * yValue)) / a1);
+        double result[] = {xValue, yValue};
+        return result;
+    }
+    std::string getCoordinates(double a1, double a2,
+      double b1, double b2, double c1, double c2) {
+        double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
+        double xValue = ((c1 - (b1 * yValue)) / a1);
+
+        // setting the string result statement
+        std::string xValueString = std::to_string(xValue);
+        std::string yValueString = std::to_string(yValue);
+
+        return "[x: " + xValueString + ", y: " + yValueString + "]";
+    }
+  protected:
+};
+
+#endif // _LINEAR_ALGEBRA_MATH_TOOLS__
