f600bba215a32ff1ed807e8925ebe764fccaa650
yashsehgal <yashsehgal.work@gmail.com>
2020-06-29 15:57:57 +0530

    type re-construction is done with all the function methods and features

--- cpp/lib/MathTools/BasicToolSet.h
+++ cpp/lib/MathTools/BasicToolSet.h
@@ -57,11 +57,37 @@ public:
       std::cout << "Choose an option from basic or advanced" << std::endl;
     }
   }
+  double addNum(int num1, int num2)
+  {
+    return num1 + num2;
+  }
+  double addNum(float num1, float num2)
+  {
+    return num1 + num2;
+  }
   double addNum(double num1, double num2)
   {
     return num1 + num2;
   }
-  double addArrayNumbers(double * testarray, int size)
+  double addArrayNumbers(int *testarray, int size)
+  {
+    double total = 0;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      total += testarray[i];
+    }
+    return total;
+  }
+  double addArrayNumbers(float *testarray, int size)
+  {
+    double total = 0;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      total += testarray[i];
+    }
+    return total;
+  }
+  double addArrayNumbers(double *testarray, int size)
   {
     double sum = 0;
     for (unsigned int i = 0; i < size; i++)
@@ -74,7 +100,7 @@ public:
   {
     return input1 + input2;
   }
-  bool addBooleanList(bool * inputList, int size)
+  bool addBooleanList(bool *inputList, int size)
   {
     bool result = false;
     for (unsigned int i = 0; i < size; i++)
@@ -93,13 +119,56 @@ public:
 
     return numberString + percentageSymbol;
   }
+  std::string getPercentage(int number)
+  {
+    // std::string percentageString = number + '%';
+    // return ;
+    std::string numberString = std::to_string(number);
+    std::string percentageSymbol = "%";
+
+    return numberString + percentageSymbol;
+  }
+  std::string getPercentage(float number)
+  {
+    // std::string percentageString = number + '%';
+    // return ;
+    std::string numberString = std::to_string(number);
+    std::string percentageSymbol = "%";
+
+    return numberString + percentageSymbol;
+  }
 
   double subtractNum(double num1, double num2)
   {
     return num1 - num2;
   }
-
-  double subtractArrayNumbers(double * arraylist, int size)
+  double subtractNum(int num1, int num2)
+  {
+    return num1 - num2;
+  }
+  double subtractNum(float num1, float num2)
+  {
+    return num1 - num2;
+  }
+  double subtractArrayNumbers(double *arraylist, int size)
+  {
+    double negTotal = 0;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      negTotal -= arraylist[i];
+    }
+    return negTotal;
+  }
+  double subtractArrayNumbers(int *arraylist, int size)
+  {
+    double negTotal = 0;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      negTotal -= arraylist[i];
+    }
+    return negTotal;
+  }
+  double subtractArrayNumbers(float *arraylist, int size)
   {
     double negTotal = 0;
     for (unsigned int i = 0; i < size; i++)
@@ -112,7 +181,33 @@ public:
   {
     return num1 * num2;
   }
-  double productArrayNumbers(double * arraylist, int size)
+  double productNum(int num1, int num2)
+  {
+    return num1 * num2;
+  }
+  double productNum(float num1, float num2)
+  {
+    return num1 * num2;
+  }
+  double productArrayNumbers(double *arraylist, int size)
+  {
+    double result = 1;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      result *= arraylist[i];
+    }
+    return result;
+  }
+  double productArrayNumbers(int *arraylist, int size)
+  {
+    double result = 1;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      result *= arraylist[i];
+    }
+    return result;
+  }
+  double productArrayNumbers(float *arraylist, int size)
   {
     double result = 1;
     for (unsigned int i = 0; i < size; i++)
@@ -125,7 +220,7 @@ public:
   {
     return input1 * input2;
   }
-  bool productBooleanList(bool * inputlist, int size)
+  bool productBooleanList(bool *inputlist, int size)
   {
     bool result = true;
     for (unsigned int i = 0; i < size; i++)
@@ -139,19 +234,79 @@ public:
   {
     return num1 / num2;
   }
-  double divideArrayNumbers(double * arraylist, int size) {
+  double divideNum(int num1, int num2)
+  {
+    return num1 / num2;
+  }
+  double divideNum(float num1, float num2)
+  {
+    return num1 / num2;
+  }
+
+  double divideArrayNumbers(double *arraylist, int size)
+  {
     double total = 1;
-    for (unsigned int i = 0; i < size; i++) {
+    for (unsigned int i = 0; i < size; i++)
+    {
       total /= arraylist[i] / total;
     }
     return total;
   }
-  std::string computePercentageFromArray(double number, double * arraylist, int size) {
+  double divideArrayNumbers(int *arraylist, int size)
+  {
+    double total = 1;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      total /= arraylist[i] / total;
+    }
+    return total;
+  }
+  double divideArrayNumbers(float *arraylist, int size)
+  {
+    double total = 1;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      total /= arraylist[i] / total;
+    }
+    return total;
+  }
+
+  std::string computePercentageFromArray(double number, double *arraylist, int size)
+  {
     double total = 0;
-    for (unsigned int i = 0; i < size; i++) {
+    for (unsigned int i = 0; i < size; i++)
+    {
       total += arraylist[i];
     }
-    double result = (number/total) * 100;
+    double result = (number / total) * 100;
+
+    std::string numberStringFormat = std::to_string(result);
+    std::string percentageSymbol = "%";
+
+    return numberStringFormat + percentageSymbol;
+  }
+  std::string computePercentageFromArray(int number, int *arraylist, int size)
+  {
+    double total = 0;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      total += arraylist[i];
+    }
+    double result = (number / total) * 100;
+
+    std::string numberStringFormat = std::to_string(result);
+    std::string percentageSymbol = "%";
+
+    return numberStringFormat + percentageSymbol;
+  }
+  std::string computePercentageFromArray(float number, float *arraylist, int size)
+  {
+    double total = 0;
+    for (unsigned int i = 0; i < size; i++)
+    {
+      total += arraylist[i];
+    }
+    double result = (number / total) * 100;
 
     std::string numberStringFormat = std::to_string(result);
     std::string percentageSymbol = "%";
@@ -159,74 +314,269 @@ public:
     return numberStringFormat + percentageSymbol;
   }
 
-  double computeMinimum(double num1, double num2) {
-    if (num1 < num2) {
+  double computeMinimum(double num1, double num2)
+  {
+    if (num1 < num2)
+    {
       return num1;
     }
-    else if (num1 == num2) {
-      return ;
+    else if (num1 == num2)
+    {
+      return;
     }
-    else {
+    else
+    {
       return num2;
     }
   }
-  double computeMinimumFromArray(double * arraylist, int size) {
+  double computeMinimum(int num1, int num2)
+  {
+    if (num1 < num2)
+    {
+      return num1;
+    }
+    else if (num1 == num2)
+    {
+      return;
+    }
+    else
+    {
+      return num2;
+    }
+  }
+  double computeMinimum(float num1, float num2)
+  {
+    if (num1 < num2)
+    {
+      return num1;
+    }
+    else if (num1 == num2)
+    {
+      return;
+    }
+    else
+    {
+      return num2;
+    }
+  }
+
+  double computeMinimumFromArray(double *arraylist, int size)
+  {
     double minValue = arraylist[0];
-    for (unsigned int i = 0; i < size; i++) {
-      if (arraylist[i] < minValue) {
+    for (unsigned int i = 0; i < size; i++)
+    {
+      if (arraylist[i] < minValue)
+      {
         minValue = arraylist[i];
       }
     }
     return minValue;
   }
-  double computeMaximum(double num1, double num2) {
-    if (num1 > num2) {
+  double computeMinimumFromArray(int *arraylist, int size)
+  {
+    double minValue = arraylist[0];
+    for (unsigned int i = 0; i < size; i++)
+    {
+      if (arraylist[i] < minValue)
+      {
+        minValue = arraylist[i];
+      }
+    }
+    return minValue;
+  }
+  double computeMinimumFromArray(float *arraylist, int size)
+  {
+    double minValue = arraylist[0];
+    for (unsigned int i = 0; i < size; i++)
+    {
+      if (arraylist[i] < minValue)
+      {
+        minValue = arraylist[i];
+      }
+    }
+    return minValue;
+  }
+
+  double computeMaximum(double num1, double num2)
+  {
+    if (num1 > num2)
+    {
       return num1;
     }
-    else if (num1 == num2) {
-      return ;
+    else if (num1 == num2)
+    {
+      return;
     }
-    else {
+    else
+    {
       return num2;
     }
   }
-  double computeMaximumFromArray(double * arraylist, int size) {
+  double computeMaximum(int num1, int num2)
+  {
+    if (num1 > num2)
+    {
+      return num1;
+    }
+    else if (num1 == num2)
+    {
+      return;
+    }
+    else
+    {
+      return num2;
+    }
+  }
+  double computeMaximum(float num1, float num2)
+  {
+    if (num1 > num2)
+    {
+      return num1;
+    }
+    else if (num1 == num2)
+    {
+      return;
+    }
+    else
+    {
+      return num2;
+    }
+  }
+
+  double computeMaximumFromArray(double *arraylist, int size)
+  {
     double maxValue = arraylist[0];
-    for (unsigned int i = 0; i < size; i++) {
-      if (arraylist[i] > maxValue) {
+    for (unsigned int i = 0; i < size; i++)
+    {
+      if (arraylist[i] > maxValue)
+      {
         maxValue = arraylist[i];
       }
     }
     return maxValue;
   }
-  double computeRectangleArea(double length, double breadth) {
+  double computeMaximumFromArray(int *arraylist, int size)
+  {
+    double maxValue = arraylist[0];
+    for (unsigned int i = 0; i < size; i++)
+    {
+      if (arraylist[i] > maxValue)
+      {
+        maxValue = arraylist[i];
+      }
+    }
+    return maxValue;
+  }
+  double computeMaximumFromArray(float *arraylist, int size)
+  {
+    double maxValue = arraylist[0];
+    for (unsigned int i = 0; i < size; i++)
+    {
+      if (arraylist[i] > maxValue)
+      {
+        maxValue = arraylist[i];
+      }
+    }
+    return maxValue;
+  }
+
+  double computeRectangleArea(double length, double breadth)
+  {
     return length * breadth;
   }
-  double computeRectanglePerimeter(double length, double breadth) {
+  double computeRectangleArea(int length, double breadth)
+  {
+    return length * breadth;
+  }
+  double computeRectangleArea(float length, double breadth)
+  {
+    return length * breadth;
+  }
+
+  double computeRectanglePerimeter(double length, double breadth)
+  {
     return 2 * (length + breadth);
   }
-  double computeSquareArea(double side) {
+  double computeRectanglePerimeter(int length, double breadth)
+  {
+    return 2 * (length + breadth);
+  }
+  double computeRectanglePerimeter(float length, double breadth)
+  {
+    return 2 * (length + breadth);
+  }
+
+  double computeSquareArea(double side)
+  {
     return side * side;
   }
-  double computeSqaurePerimeter(double side) {
+  double computeSquareArea(int side)
+  {
+    return side * side;
+  }
+  double computeSquareArea(float side)
+  {
+    return side * side;
+  }
+
+  double computeSqaurePerimeter(double side)
+  {
     return 4 * side;
   }
-  bool checkEven(int number) {
-    if (number % 2 == 0) {
+  double computeSqaurePerimeter(int side)
+  {
+    return 4 * side;
+  }
+  double computeSqaurePerimeter(float side)
+  {
+    return 4 * side;
+  }
+
+  bool checkEven(int number)
+  {
+    if (number % 2 == 0)
+    {
       return true;
     }
-    else {
+    else
+    {
       return false;
     }
   }
-  bool checkOdd(int number) {
-    if (number % 2 == 0) {
+  bool checkEven(long int number)
+  {
+    if (number % 2 == 0)
+    {
+      return true;
+    }
+    else
+    {
       return false;
     }
-    else {
+  }
+  bool checkOdd(int number)
+  {
+    if (number % 2 == 0)
+    {
+      return false;
+    }
+    else
+    {
       return true;
     }
   }
+  bool checkOdd(long int number)
+  {
+    if (number % 2 == 0)
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+
 protected:
 };
 
