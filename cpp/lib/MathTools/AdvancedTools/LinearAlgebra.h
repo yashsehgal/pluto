c2b7a8b15753abd77eb8e584402c62a4fcbd3d6b
yashsehgal <yashsehgal.work@gmail.com>
2020-07-13 21:15:58 +0530

    new datahandling features has been added in the module --MathTools-LinearAlgebra(cpp version) --initial-feature

--- cpp/lib/MathTools/AdvancedTools/LinearAlgebra.h
+++ cpp/lib/MathTools/AdvancedTools/LinearAlgebra.h
@@ -16,13 +16,27 @@ class LinearAlgebra {
         std::cout << "use valid mode!" << std::endl;
       }
     }
-    double *getCoordinates_XY(double a1, double a2,
+    double * getCoordinates_XY(double a1, double a2,
       double b1, double b2, double c1, double c2) {
         double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
         double xValue = ((c1 - (b1 * yValue)) / a1);
         double result[] = {xValue, yValue};
         return result;
     }
+    double * getCoordinates_XY(int a1, int a2,
+      int b1, int b2, int c1, int c2) {
+      double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
+      double xValue = ((c1 - (b1 * yValue)) / a1);
+      double result[] = {xValue, yValue};
+      return result;
+    }
+    double * getCoordinates_XY(float a1, float a2,
+      float b1, float b2, float c1, float c2) {
+      double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
+      double xValue = ((c1 - (b1 * yValue)) / a1);
+      double result[] = {xValue, yValue};
+      return result;
+      }
     std::string getCoordinates(double a1, double a2,
       double b1, double b2, double c1, double c2) {
         double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
