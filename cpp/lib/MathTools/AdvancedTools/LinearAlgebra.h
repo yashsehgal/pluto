cdccc391b0ff977f16ed889996097601ca0a29cf
yashsehgal <yashsehgal.work@gmail.com>
2020-07-27 17:31:35 +0530

    codebase updated

--- cpp/lib/MathTools/AdvancedTools/LinearAlgebra.h
+++ cpp/lib/MathTools/AdvancedTools/LinearAlgebra.h
@@ -6,45 +6,45 @@
 
 class LinearAlgebra {
   private:
-    char *mode;
+    char *mode{};
   public:
-    LinearAlgebra(char * command = "basic") {
-      if (mode == "advanced") {
+    explicit LinearAlgebra(char * command = "basic") {
+      if (command == "advanced") {
         mode = command;
       }
       else {
         std::cout << "use valid mode!" << std::endl;
       }
     }
-    double * getCoordinates_XY(double a1, double a2,
-      double b1, double b2, double c1, double c2) {
-        double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
-        double xValue = ((c1 - (b1 * yValue)) / a1);
-        double result[] = {xValue, yValue};
+    static const double * getCoordinates_XY(const double a1, const double a2,
+      const double b1, const double b2, const double c1, const double c2) {
+        const double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
+        const double xValue = ((c1 - (b1 * yValue)) / a1);
+        const double result[] = {xValue, yValue};
         return result;
     }
-    double * getCoordinates_XY(int a1, int a2,
-      int b1, int b2, int c1, int c2) {
-      double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
-      double xValue = ((c1 - (b1 * yValue)) / a1);
-      double result[] = {xValue, yValue};
+    static const double * getCoordinates_XY(const int a1, const int a2,
+      const int b1, const int b2, const int c1, const int c2) {
+      const double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
+      const double xValue = ((c1 - (b1 * yValue)) / a1);
+      const double result[] = {xValue, yValue};
       return result;
     }
-    double * getCoordinates_XY(float a1, float a2,
-      float b1, float b2, float c1, float c2) {
-      double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
-      double xValue = ((c1 - (b1 * yValue)) / a1);
-      double result[] = {xValue, yValue};
+    static const double * getCoordinates_XY(const float a1, const float a2,
+      const float b1, const float b2, const float c1, const float c2) {
+      const double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
+      const double xValue = ((c1 - (b1 * yValue)) / a1);
+      const double result[] = {xValue, yValue};
       return result;
       }
-    std::string getCoordinates(double a1, double a2,
-      double b1, double b2, double c1, double c2) {
-        double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
-        double xValue = ((c1 - (b1 * yValue)) / a1);
+    static const std::string getCoordinates(const double a1, const double a2,
+      const double b1, const double b2, const double c1, const double c2) {
+        const double yValue = (((a1 * c2) - (a2 * c1)) / ((a2 * b1) + (a1 * b2)));
+        const double xValue = ((c1 - (b1 * yValue)) / a1);
 
         // setting the string result statement
-        std::string xValueString = std::to_string(xValue);
-        std::string yValueString = std::to_string(yValue);
+        const std::string xValueString = std::to_string(xValue);
+        const std::string yValueString = std::to_string(yValue);
 
         return "[x: " + xValueString + ", y: " + yValueString + "]";
     }
--- cpp/lib/MathTools/BooleanAlgebra.h
+++ cpp/lib/MathTools/BooleanAlgebra.h
@@ -3,8 +3,18 @@
 
 class BooleanAlgebra {
   private:
+  bool permission;
   public:
+  explicit BooleanAlgebra(const bool);
+  static const bool addBoolean(const bool input1, const bool input2) {
+    return input1 + input2;
+  }
   protected:
 };
+
+explicit BooleanAlgebra::BooleanAlgebra(bool activation = false) {
+  if (activation == true) permission = activation;
+}
+
 
 #endif // _MATH_TOOLS__BOOLEAN_ALGEBRA__
