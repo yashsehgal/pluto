49877c111af7a83c5966894582073d3462d237bc
yashsehgal <yashsehgal.work@gmail.com>
2020-07-27 19:32:21 +0530

    boolean methods are added in the module

--- cpp/lib/MathTools/BooleanAlgebra.h
+++ cpp/lib/MathTools/BooleanAlgebra.h
@@ -3,17 +3,33 @@
 
 class BooleanAlgebra {
   private:
-  bool permission;
+  static bool permission;
   public:
   explicit BooleanAlgebra(const bool);
-  static const bool addBoolean(const bool input1, const bool input2) {
-    return input1 + input2;
+  static const bool addBoolean(const bool, const bool);
+  static const bool productBoolean(const bool, const bool);
+  static const bool getCompliment(const bool);
+  static void handleActivationError() {
+    std::cout << "MethodNotActivated: Error Handler Says" << std::endl;
   }
   protected:
 };
 
 explicit BooleanAlgebra::BooleanAlgebra(bool activation = false) {
   if (activation == true) permission = activation;
+}
+
+const bool BooleanAlgebra::addBoolean(const bool input1, const bool input2) {
+  if (permission == true) return input1 + input2;
+  else BooleanAlgebra::handleActivationError();
+}
+const bool BooleanAlgebra::productBoolean(const bool input1, const bool input2) {
+  if (permission == true) return input1 * input2;
+  else BooleanAlgebra::handleActivationError();
+}
+const bool BooleanAlgebra::getCompliment(const bool input) {
+  if (permission == true) return !input;
+  else BooleanAlgebra::handleActivationError();
 }
 
 
